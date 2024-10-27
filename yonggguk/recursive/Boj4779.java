ee07b26b2fc5b21056809a8118dfc43b6f755248
User <yonguk929@gmail.com>
2024-10-27 23:39:46 +0900

    add : Boj4779(재귀/규칙)

---
+++ yonggguk/recursive/Boj4779.java
@@ -0,0 +1,26 @@
+package recursive;
+
+import java.util.Scanner;
+
+public class Boj4779 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        int n;
+
+        while (sc.hasNext()) {
+            n = sc.nextInt();
+            for (int i = 0; i < n + 1; i++) {
+                if (i == 0) {
+                    str = "-";
+                } else {
+                    str = str + " ".repeat(str.length()) + str;
+                }
+            }
+            System.out.println(str);
+        }
+
+    }
+
+}
